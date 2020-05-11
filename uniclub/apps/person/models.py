4ec61f1056c116c06e59fa738e736bed47656650
boddah <adilbekk8@gmail.com>
2020-05-12 04:12:40 +0600

    Person: coach init

--- uniclub/apps/person/models.py
+++ uniclub/apps/person/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from apps.clubs.models import Club
+
+
+class Coach(models.Model):
+    class Meta:
+        verbose_name = "Тренеры"
+        verbose_name_plural = "Тренер"
+
+    club = models.ForeignKey(
+        Club, related_name="coaches", on_delete=models.CASCADE
+    )
+    full_name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to="coach/")
+
+    def __str__(self):
+        return self.full_name
