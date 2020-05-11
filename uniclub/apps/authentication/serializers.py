cadcc2b7645244355621cbd016be7adfd0e8b6bc
boddah <adilbekk8@gmail.com>
2020-05-12 04:43:20 +0600

    Auth: added auth views

---
+++ uniclub/apps/authentication/serializers.py
@@ -0,0 +1,42 @@
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.serializers import (
+    TokenObtainPairSerializer as BaseTokenObtainPairSerializer,
+)
+
+
+class TokenObtainPairSerializer(BaseTokenObtainPairSerializer):
+    username_field = "username"
+
+    def validate(self, attrs):
+        try:
+            attrs["request"] = self.context["request"]
+        except KeyError:
+            pass
+
+        self.user = authenticate(**attrs)
+        if self.user is None or not self.user.is_active:
+            raise AuthenticationFailed(
+                self.error_messages["no_active_account"], "no_active_account",
+            )
+        refresh = self.get_token(self.user)
+
+        data = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+
+        return data
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["my_secret"] = "voyager-1"
+
+        return token
+
+
+class TokenOutputSerializer(serializers.Serializer):
+    access = serializers.CharField()
+    refresh = serializers.CharField()
--- uniclub/apps/urls.py
+++ uniclub/apps/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 
 urlpatterns = [
+    path("auth/", include("apps.authentication.urls")),
     path("website/", include("apps.website.urls")),
     path("products/", include("apps.products.urls")),
     path("clubs/", include("apps.clubs.urls")),
