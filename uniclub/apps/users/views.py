83f284094b951095753f6d14b5397dd5cf9d8508
boddah <adilbekk8@gmail.com>
2020-05-13 04:25:29 +0600

    Users: added partnerAPIMixin

--- uniclub/apps/users/views.py
+++ uniclub/apps/users/views.py
@@ -1,3 +1,26 @@
+from typing import List, Optional
+
 from django.shortcuts import render
 
-# Create your views here.
+from apps.clubs.models import Club
+
+from .permissions import IsPartner
+
+
+class PartnerAPIMixin:
+    permission_classes = (IsPartner,)
+    related_fields: List[str] = []
+    _club: Optional[Club] = None
+
+    @property
+    def club(self):
+        if not self._club:
+            user = self.request.user
+            if hasattr(user, "club"):
+                self._club = user.club
+        return self._club
+
+    def get_object(self):
+        if not self.club:
+            return None
+        return self._club
