a577a6fc4f31c9186311f85e65761698d805a41c
boddah <adilbekk8@gmail.com>
2020-05-13 16:52:41 +0600

    Products: added publicAPIMixin

--- uniclub/apps/products/views.py
+++ uniclub/apps/products/views.py
@@ -1,9 +1,11 @@
 from rest_framework.generics import ListAPIView
 
+from apps.core.views import PublicAPIMixin
+
 from .models import UniPass
 from .serializers import UniPassSerializer
 
 
-class UniPassListView(ListAPIView):
+class UniPassListView(PublicAPIMixin, ListAPIView):
     serializer_class = UniPassSerializer
     queryset = UniPass.objects.all()
