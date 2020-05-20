44636c421ce2224827d73f14d5fa00212316ee29
boddah <adilbekk8@gmail.com>
2020-05-21 00:27:16 +0600

    WebSite: added publicapimixin to faq views

--- uniclub/apps/website/views.py
+++ uniclub/apps/website/views.py
@@ -1,21 +1,22 @@
 from rest_framework.generics import ListAPIView, CreateAPIView
 
+from apps.core.views import PublicAPIMixin
 from .models import FAQ, FeedBack, PartnerFeedBack
 from .serializers import (
     FAQSerializer, FeedBackSerializer, PartnerFeedBackSerializer
 )
 
 
-class FAQListView(ListAPIView):
+class FAQListView(PublicAPIMixin, ListAPIView):
     serializer_class = FAQSerializer
     queryset = FAQ.objects.all()
 
 
-class FeedBackPostView(CreateAPIView):
+class FeedBackPostView(PublicAPIMixin, CreateAPIView):
     serializer_class = FeedBackSerializer
     queryset = FeedBack.objects.all()
 
 
-class PartnerFeedBackView(CreateAPIView):
+class PartnerFeedBackView(PublicAPIMixin, CreateAPIView):
     serializer_class = PartnerFeedBackSerializer
     queryset = PartnerFeedBack.objects.all()
