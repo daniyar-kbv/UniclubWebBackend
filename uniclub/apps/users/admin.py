701194cba8f7aca51e783cdecdcdb1c7b132b784
boddah <adilbekk8@gmail.com>
2020-05-30 11:53:10 +0600

    moved requirements.txt, added missed migrations, fixed user admin page

--- uniclub/apps/users/admin.py
+++ uniclub/apps/users/admin.py
@@ -1,9 +1,35 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext as _
 
 User = get_user_model()
 
 
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'middle_name')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser'),
+        }),
+        (_('Type'), {'fields': ('user_type',)}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ()
+    filter_horizontal = ()
+
+
+
 admin.site.unregister(Group)
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
