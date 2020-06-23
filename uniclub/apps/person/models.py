220d3ff86fe614edfc4c6b75cd6450e3a326973e
adilbekk8 <adilbekk8@gmail.com>
2020-06-23 20:21:55 +0600

    Person: minor changes in models

--- uniclub/apps/person/models.py
+++ uniclub/apps/person/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from phonenumber_field.modelfields import PhoneNumberField
 
 from apps.clubs.models import Club
 from apps.core.models import NameModel, CityModel
 
 from . import Gender
+
+User = get_user_model()
 
 
 class Coach(NameModel):
@@ -22,6 +25,16 @@ class Coach(NameModel):
 
 
 class ClientProfile(models.Model):
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профиль"
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile",
+        verbose_name="Пользователь"
+    )
     city = models.ForeignKey(
         CityModel, on_delete=models.SET_NULL, null=True, verbose_name="Город"
     )
@@ -31,8 +44,15 @@ class ClientProfile(models.Model):
     sex = models.CharField("Пол", choices=Gender.choices, max_length=20)
     birth_date = models.DateTimeField(null=True, blank=True)
 
+    def __str__(self):
+        return self.user.full_name
+
 
 class ClientChildren(NameModel):
+    class Meta:
+        verbose_name = "Ребенок"
+        verbose_name_plural = "Ребенок"
+
     parent = models.ForeignKey(
         ClientProfile,
         verbose_name="Родитель",
@@ -47,3 +67,6 @@ class ClientChildren(NameModel):
     city = models.ForeignKey(
         CityModel, on_delete=models.SET_NULL, null=True, verbose_name="Город"
     )
+
+    def __str__(self):
+        return self.full_name
