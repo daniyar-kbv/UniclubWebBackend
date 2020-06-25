b09043fb0fe10cdd70beba7d48b6cb34e3600dd6
adilbekk8 <adilbekk8@gmail.com>
2020-06-25 19:06:34 +0600

    Products: alter unipass

--- uniclub/apps/products/models.py
+++ uniclub/apps/products/models.py
@@ -6,7 +6,7 @@ class Benefits(models.Model):
         verbose_name = "Преимущество"
         verbose_name_plural = "Преимущества"
 
-    name = models.CharField(max_length=256)
+    name = models.CharField("Текст", max_length=256)
 
     def __str__(self):
         return self.name
@@ -17,11 +17,11 @@ class UniPass(models.Model):
         verbose_name = "Тариф"
         verbose_name_plural = "Тарифы"
 
-    name = models.CharField(max_length=56)
-    price = models.DecimalField(max_digits=12, decimal_places=2)
-    days_amount = models.PositiveSmallIntegerField(default=0)
-    visits_amount = models.PositiveSmallIntegerField(default=0)
-    benefits = models.ManyToManyField(Benefits)
+    name = models.CharField("Название", max_length=56)
+    price = models.DecimalField("Цена", max_digits=12, decimal_places=2)
+    days_amount = models.PositiveSmallIntegerField("Количество дней", default=0)
+    visits_amount = models.PositiveSmallIntegerField("Количество посещении", default=0)
+    benefits = models.ManyToManyField(Benefits, verbose_name="Преимущества", null=True, blank=True)
 
     def __str__(self):
         return self.name
