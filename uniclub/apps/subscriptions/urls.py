bebe100b56eeb301092d058bc6101096a3b8ca15
boddah <adilbekk8@gmail.com>
2020-07-06 00:47:53 +0600

    Subscriptions: added freeze request view

--- uniclub/apps/subscriptions/urls.py
+++ uniclub/apps/subscriptions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import SubscribeView
+from .views import SubscribeView, FreezeRequestView
 
 urlpatterns = [
     path("registration", SubscribeView.as_view()),
+    path("<uuid:uuid>/freeze", FreezeRequestView.as_view()),
 ]
--- uniclub/apps/subscriptions/views.py
+++ uniclub/apps/subscriptions/views.py
@@ -1,12 +1,23 @@
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
 
 from apps.users.views import ClientAPIMixin
 
-from .models import Subscription
+from .models import Subscription, FreezeRequest
 from .serializers import SubscriptionCreateSerializer
 
 
 class SubscribeView(ClientAPIMixin, CreateAPIView):
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionCreateSerializer
+
+
+class FreezeRequestView(ClientAPIMixin, APIView):
+    def post(self, request, uuid):
+        FreezeRequest.objects.create(
+            customer=request.user,
+            subscription_id=uuid
+        )
+
+        return Response(status=201)
